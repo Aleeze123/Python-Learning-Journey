09b9001e91ec54aeeea3b4a0f7bdcc3443b4abba
Aleeze <alizey649@gmail.com>
2025-02-24 00:07:27 +0500

    concatenation..

---
+++ concatenation.py
@@ -0,0 +1,14 @@
+# concatenation
+first_Name = "Hamzah"
+last_Name = "Syed"
+
+full_Name = first_Name + " " + last_Name
+print(full_Name)
+
+
+# F-strings provide a concise way to include variables in a string.
+
+
+first_Name = "Hamzah"
+last_Name = "Syed"
+print(f"{first_Name} {last_Name}")
