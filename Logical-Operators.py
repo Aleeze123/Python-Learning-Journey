611b08f1d0aaa2b7f8ac590ffbfb68b7cf7bc581
Aleeze <alizey649@gmail.com>
2025-02-26 00:49:47 +0500

    Logical Operators

---
+++ Logical-Operators.py
@@ -0,0 +1,15 @@
+# ------------------------------- Logical Operators --------------------------------------------------
+# Logical operators are used to combine conditional statements
+
+# and 
+x = 7
+print(x > 4  and x < 10) #True
+
+# or
+x= 8
+print(x > 9 or x < 2) #False
+
+# not
+x = 5
+
+print(not(x > 3 and x < 10))
