fe6afdd190328b4e7026f929c3d82a7f250e25b1
Aleeze <alizey649@gmail.com>
2025-02-26 00:31:12 +0500

    Assignment Operators

---
+++ Assignment-Operators.py
@@ -0,0 +1,69 @@
+# ------------------------------- Assignment Operators --------------------------------------------------
+# Assignment operators are used to assign values to variables
+# =
+x=7
+print(x) #7
+
+# +=
+x=7
+x +=3
+print(x) #10
+
+# -=
+x=7
+x -= 2
+print(x) #5
+
+# *=
+x= 7
+x*= 3
+print(x) #21
+
+# /=
+x= 7
+x/= 2
+print(x) #3.5
+
+# %=
+x=7
+x%=4
+print(x) #3
+
+# //=
+x=7
+x//=2
+print(x) #3
+
+# **=
+x=7
+x**=5
+print(x) #16807
+
+# &=
+x=7
+x&=9
+print(x) #1
+
+# |=
+x=7
+x|=5
+print(x)#7
+
+# ^=
+x=5
+x^=3
+print(x) #6
+
+# >>=
+x=5
+x>>=3
+print(x) #0
+
+# <<=
+x=5
+x<<=3
+print(x) #40
+
+# :=
+print(x := 9) #9
+
