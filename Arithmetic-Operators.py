d4f6df5fd3b6309ddbdaac89dba409062d9a7f04
Aleeze <alizey649@gmail.com>
2025-02-26 00:07:07 +0500

    Arithmetic Operators

---
+++ Arithmetic-Operators.py
@@ -0,0 +1,12 @@
+# ------------------------------- Arithmetic Operators --------------------------------------------------
+# Arithmetic operators are used with numeric values to perform common mathematical operations
+x=9
+y=2
+print(x + y) # Addition
+print(x - y) # Subtraction
+print(x * y) # Multiplication
+print(x / y) # Division
+print(x % y ) #	Modulus
+print(x ** y) # Exponentiation
+print(x // y) # Floor division
+ 
